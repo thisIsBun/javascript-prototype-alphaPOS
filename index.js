248bbfef2953a7241b70d31fa1b4549b718fa984
thisIsBun <bun.coding@gmail.com>
2023-12-24 17:52:33 +0800

    setup Drink constructor

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function Drink(name, ice, sugar) {
+  this.name = name;
+  this.ice = ice;
+  this.sugar = sugar;
+}
 
+Drink.prototype.price = function () {
+  switch (this.name) {
+    case "Black Tea":
+    case "Oolong Tea":
+    case "Baozong Tea":
+    case "Green Tea":
+      return 30;
+    case "Bubble Milk Tea":
+    case "Lemon Green Tea":
+      return 50;
+    case "Black Tea Latte":
+    case "Matcha Latte":
+      return 55;
+    default:
+      alert("No this drink");
+  }
+};
